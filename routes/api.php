cd436923eafb8a66907d37e8f499aa2f453a7f41
Gustavo Antunes <50056058+gustavo4ntunes@users.noreply.github.com>
2024-08-30 11:18:45 -0300

    Update api.php
    creating routes

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,7 @@ Route::post('/cadastrar', [UserController::class, 'store']);
 
 Route::prefix('/user')->group(function (){
     Route::get('/', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
 });
